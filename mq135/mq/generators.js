b136e087c9e12ad91c19f4de5d6db9904f160ba9
MrBrown6210 <36806221+MrBrown6210@users.noreply.github.com>
2021-01-25 14:06:14 +0700

    Update generators.js

--- mq135/mq/generators.js
+++ mq135/mq/generators.js
@@ -1,21 +1,11 @@
-Blockly.JavaScript['mq.random-text'] = function (block) {
-  return [`DEV_IO.mq().random()`, Blockly.JavaScript.ORDER_ATOMIC]
-}
-
-Blockly.JavaScript['mq.basic_string'] = function (block) {
-  return [`DEV_IO.mq().random()`, Blockly.JavaScript.ORDER_ATOMIC]
-}
-
-Blockly.JavaScript['mq.recieved'] = function(block) {
-	var statement = Blockly.JavaScript.statementToCode(block, 'DO');
-	return 'DEV_IO.mq().callback([](){'+statement+'});\n';
+Blockly.JavaScript["mq.random-text"] = function (block) {
+  return [`DEV_IO.mq().random()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['mq.didEnter'] = function(block) {
-	var statement = Blockly.JavaScript.statementToCode(block, 'DO');
-	return 'DEV_IO.mq().didEnter([](){'+statement+'});\n';
+Blockly.JavaScript["mq.basic_string"] = function (block) {
+  return [`DEV_IO.mq().random()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['mq.data'] = function (block) {
-  return [`DEV_IO.mq().distance()`];
+Blockly.JavaScript["mq.data"] = function (block) {
+  return [`DEV_IO.mq().value()`];
 };
